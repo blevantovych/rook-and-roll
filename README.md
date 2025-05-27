bb05ad1862f0c6c4c39c7dca526d18bb3bf05e49
blevantovych <levantovychbogdan@gmail.com>
2025-05-27 21:51:06 +0300

    add gif

---
+++ README.md
@@ -0,0 +1,4 @@
+♟️ RockAndRoll
+RockAndRoll is a web application that allows users to solve chess puzzles and create their own. Whether you're sharpening your tactical skills or designing unique challenges for others, BlunderBuddy provides a simple, interactive, and engaging experience for chess enthusiasts of all levels.
+
+![RockAndRoll Preview](./public/rock_and_roll.gif)
--- index.js
+++ index.js
@@ -114,8 +114,8 @@ function inputHandler(event) {
   }
 }
 
-// const puzzleInput = '00sJ9,r3r1k1/p4ppp/2p2n2/1p6/3P1qb1/2NQR3/PPB2PP1/R1B3K1 w - - 5 18,e3g3 e8e1 g1h2 e1c1 a1c1 f4h6 h2g1 h6c1,2671,105,87,325,advantage attraction fork middlegame sacrifice veryLong,https://lichess.org/gyFeQsOE#35,French_Defense French_Defense_Exchange_Variation'
-const puzzleInput = '00sO1,1k1r4/pp3pp1/2p1p3/4b3/P3n1P1/8/KPP2PN1/3rBR1R b - - 2 31,b8c7 e1a5 b7b6 f1d1,998,85,94,293,advantage discoveredAttack master middlegame short,https://lichess.org/vsfFkG0s/black#62'
+const puzzleInput = '00sJ9,r3r1k1/p4ppp/2p2n2/1p6/3P1qb1/2NQR3/PPB2PP1/R1B3K1 w - - 5 18,e3g3 e8e1 g1h2 e1c1 a1c1 f4h6 h2g1 h6c1,2671,105,87,325,advantage attraction fork middlegame sacrifice veryLong,https://lichess.org/gyFeQsOE#35,French_Defense French_Defense_Exchange_Variation'
+// const puzzleInput = '00sO1,1k1r4/pp3pp1/2p1p3/4b3/P3n1P1/8/KPP2PN1/3rBR1R b - - 2 31,b8c7 e1a5 b7b6 f1d1,998,85,94,293,advantage discoveredAttack master middlegame short,https://lichess.org/vsfFkG0s/black#62'
 
 const puzzlePieces = puzzleInput.split(',')
 const puzzle = {
